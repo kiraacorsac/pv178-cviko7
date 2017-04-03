c5a72e406f7e6878e902f462a5f7a5a8a7f2f847
Milan Mikus <mylan719@hotmail.com>
2017-04-03 17:33:36 +0200

    Added solutions

---
+++ FilesAndFolders/Solution.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using FilesAndFolders.SampleData;
+
+namespace FilesAndFolders
+{
+    public static class Solution
+    {
+        public static void Tasks()
+        {
+            FileEqualityComparer();
+
+            FileSystemWatcher();
+        }
+
+        private static void FileSystemWatcher()
+        {
+            /* TODO 
+             Pomocí třídy FileSystemWatcher sledujte složku SampleData v projektu FilesAndFolders.
+             V okamžiku když dojde ve složce k vytvoření nového souboru s příponou .txt, 
+             vypište do konzole vhodné hlášení (ostatní typy souborů ignorujte).
+             V rámci testování v dané složce nejprve vytvořte soubor "text5.txt", dále pak "data.xml" 
+             (nemělo by dojít k vyvolání obsluhy události Created třídy FileSystemWatcher)*/
+
+            var fileWatcher = new FileSystemWatcher
+            {
+                Path = Paths.SampleDataFolder,
+                NotifyFilter = NotifyFilters.FileName,
+                Filter = "*.txt"
+            };
+            fileWatcher.Created += FileWatcher_Created;
+            fileWatcher.EnableRaisingEvents = true;
+
+            var text5FilePath = Path.Combine(Paths.SampleDataFolder, "text5.txt");
+            File.Create(text5FilePath).Dispose();
+            var xmlFilePath = Path.Combine(Paths.SampleDataFolder, "data.xml");
+            File.Create(xmlFilePath).Dispose();
+            File.Delete(text5FilePath);
+            File.Delete(xmlFilePath);
+
+            fileWatcher.Created -= FileWatcher_Created;
+            fileWatcher.Dispose();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private static void FileEqualityComparer()
+        {
+            /* TODO 
+            Vytvořte metodu, která bude brát jako parametry cesty ke dvěma souborům, 
+            které pak porovná (zjistí, zda jsou soubory identické). 
+            V případě že jsou totožné, vrátí true, jinak false.
+            Testování proveďte nejdříve na souborech text1.txt a text2.txt (false)
+            následně pak na text1.txt a text3.txt (metoda by měla vrátit true)*/
+
+            var areDifferentFilesEqual = FileEquals(Paths.Text1, Paths.Text2);
+            var areSameFilesEqual = FileEquals(Paths.Text1, Paths.Text3);
+        }
+
+
+        private static bool FileEquals(string path1, string path2)
+        {
+            var file1 = File.ReadAllBytes(path1);
+            var file2 = File.ReadAllBytes(path2);
+            return file1.Length == file2.Length && !file1.Where((t, i) => t != file2[i]).Any();
+        }
+
+        private static void FileWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Creation of new txt file: {e.Name} detected.");
+        }
+    }
+}
---
+++ LinqToXml/Solution.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+using LinqToXml.SampleData;
+
+namespace LinqToXml
+{
+    public static class Solution
+    {
+        public static void Tasks()
+        {
+            var moviesRoot = XElement.Load(Paths.Movies);
+            var movieRatingssRoot = XElement.Load(Paths.MovieRatings);
+
+            /* TODO 
+              Za pomocí Linq to Xml zjistěte všechny žánry filmů, které umístěte do jednoho řetězce a oddělte čárkami
+             */
+            var allGenres = moviesRoot
+                .Elements("Movie")
+                .Descendants("Genre")
+                .Select(element => element.Value)
+                .Aggregate((first, second) => $"{first}, {second}");
+            Console.WriteLine($"All genres: {allGenres}" + Environment.NewLine);
+            
+            /* TODO 
+              Za pomocí Linq to Xml spočítejte průměrné hodnocení všech filmů
+             */
+            var averageRating = movieRatingssRoot
+                .Elements("Movie")
+                .Descendants("Score")
+                .Select(scoreElement =>
+                {
+                    var rating = double.Parse(scoreElement.Value.Replace(',', '.'), CultureInfo.InvariantCulture);
+                    if (Math.Abs(rating % 1) < 0.00000001)
+                    {
+                        return rating / 100.0;
+                    }
+                    return rating / 10.0;
+                })
+                .Average();
+            Console.WriteLine($"Average movie rating: {averageRating:N2}" + Environment.NewLine);
+
+            /* TODO 
+              Za pomocí Linq to Xml získejte z Movies.xml a MovieRatings.xml následující údaje:
+                název filmu, 
+                jméno režiséra (spojte jméno a příjmení), 
+                veškerá hodnocení (stačí ve formě List<string>, do výsledku zahrnujte pouze filmy, které mají alespoň jedno hodnocení) 
+             */
+            var moviesWithRatings = moviesRoot.Elements("Movie")
+                .Join(movieRatingssRoot.Elements("Movie"), movieElement => movieElement.FirstAttribute.Value,
+                    movieRatingElement => movieRatingElement.FirstAttribute.Value,
+                    (movieElement, movieRatingElement) =>
+                        new
+                        {
+                            MovieName = movieElement.FirstAttribute.Value,
+                            Director = movieElement.Element("Director")?.Descendants()
+                                    .Select(directorElement => directorElement.Value)
+                                    .Aggregate((first, second) =>  $"{first} {second}"),
+                            Ratings = movieRatingElement.Descendants("Score").Select(scoreElement => scoreElement.Value).ToList()
+                        }).ToList();
+
+            var movies = moviesWithRatings
+                .Select(movie => movie.MovieName)
+                .Aggregate((first, second) => $"{first}, {second}");
+            Console.WriteLine($"Movies with rating: {movies}" + Environment.NewLine);
+
+            /* TODO 
+              I.    Do Movies.xml přidejte xml element "Movie" s filmem "Pulp Fiction"*, jako režiséra uveďte Quentina Tarantina, 
+                    žánr nastavte na Thriller, jako rok vydání uveďte 1994 a vytvořte prozatím prázdný element "Cast"
+                    * název filmu uveďte jako hodnotu atributu "MovieName" elementu "Movie"
+
+              II.   Dovnitř elementu "Cast" vložte všechny herce z ostatních filmů (zachovejte strukturu elementu "Actor")
+
+              III.  Při kontrole správnosti údajů se zjistilo, že kromě herce Samuela L. Jacksona v Pulp Fiction žádný z 
+                    uvedených herců nehraje, smažte tedy všechny herce, kteří ve filmu neučinkovali (použijte LinqToXml).
+
+              IV.   Element s filmem "Pulp Fiction" vypište na konzoly.
+
+             */
+
+            // I.
+            moviesRoot.Add(
+                new XElement("Movie", 
+                    new XElement("Director", new XElement("FistName", "Quentin"), new XElement("LastName", "Tarantino")), 
+                    new XElement("Genres", new XElement("Genre", "Thriller")), 
+                    new XElement("ReleaseYear", 1994),
+                    new XElement("Cast")));
+            var pulpFiction = moviesRoot.Elements().Skip(1).First();
+            pulpFiction.SetAttributeValue("MovieName", "Pulp Fiction");
+
+            // II.
+            var actors = moviesRoot.Descendants("Actor").ToList();
+            pulpFiction.Element("Cast")?.Add(actors);
+
+            // III.
+            pulpFiction.Element("Cast")?.Elements()
+                .Where(element =>
+                {
+                    var firstName = element.Element("FirstName");
+                    var lastName = element.Element("LastName");
+                    return lastName != null && firstName != null && 
+                       (!firstName.Value.Equals("Samuel") || !lastName.Value.Equals("Jackson"));
+                }).Remove();
+            Console.WriteLine(Environment.NewLine + "Pulp Fiction movie element:");
+
+            // IV.
+            Console.WriteLine(pulpFiction);
+        }
+    }
+}
